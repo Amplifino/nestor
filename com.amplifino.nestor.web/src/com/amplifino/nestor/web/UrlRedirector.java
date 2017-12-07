eed57a1853d9de893cd791e8e982c4d4c52baa3b
Karel Haeck <karel.haeck@amplifino.com>
2017-12-07 10:13:22 +0100

    renamed HomePageRedirector to UrlRedirector and made factory config

--- com.amplifino.nestor.web/src/com/amplifino/nestor/web/UrlRedirector.java
+++ com.amplifino.nestor.web/src/com/amplifino/nestor/web/UrlRedirector.java
@@ -11,20 +11,21 @@ import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.ConfigurationPolicy;
 import org.osgi.service.http.whiteboard.HttpWhiteboardConstants;
+import org.osgi.service.metatype.annotations.AttributeDefinition;
 import org.osgi.service.metatype.annotations.Designate;
 import org.osgi.service.metatype.annotations.ObjectClassDefinition;
 
 @Component(service=Servlet.class, 
 	property= { HttpWhiteboardConstants.HTTP_WHITEBOARD_SERVLET_PATTERN + "="}, 
 	configurationPolicy=ConfigurationPolicy.REQUIRE)
-@Designate(ocd=HomePageRedirector.HomePageRedirectorConfig.class)
-public class HomePageRedirector extends HttpServlet {
+@Designate(ocd=UrlRedirector.UrlRedirectorConfig.class, factory=true)
+public class UrlRedirector extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
 	private String redirectURL;
 
 	@Activate
-	public void activate(HomePageRedirectorConfig config) {
+	public void activate(UrlRedirectorConfig config) {
 		this.redirectURL = config.redirectURL();
 	}
 	
@@ -33,8 +34,12 @@ public class HomePageRedirector extends HttpServlet {
 		resp.sendRedirect(resp.encodeRedirectURL(redirectURL));
 	}
 	
-	@ObjectClassDefinition(name="Home Page")
-	@interface HomePageRedirectorConfig {
+	@ObjectClassDefinition(name="URL Redirection")
+	@interface UrlRedirectorConfig {
+		@AttributeDefinition(description="URL to redirect, leave blank for home page")
+		String osgi_http_whiteboard_servlet_pattern() default "";
 		String redirectURL();
+		String webconsole_configurationFactory_nameHint() 
+		default "Redirect {osgi.http.whiteboard.servlet.pattern} to {redirectURL}";
 	}
 }
