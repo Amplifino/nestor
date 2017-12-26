a947155dd51b7d81798a9ae5f9bd4b560b998d47
Karel Haeck <karel.haeck@amplifino.com>
2017-12-26 12:50:26 +0100

    swagger initial version

---
+++ com.amplifino.nestor.swagger/src/com/amplifino/nestor/swagger/ApiListing.java
@@ -0,0 +1,92 @@
+package com.amplifino.nestor.swagger;
+
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.jaxrs.Reader;
+import io.swagger.models.Swagger;
+import io.swagger.util.Yaml;
+
+@Path("/")
+public class ApiListing {
+
+	@Context
+	private SwaggerApplication swaggerApplication;
+	
+	private Optional<Swagger> process(String alias) {
+		return swaggerApplication.getApplication(alias)
+			.map(application -> this.toSwagger(alias, application));
+	}
+	
+	private Swagger toSwagger(String alias, Application application) {				
+		return new Reader(new Swagger().basePath("api/" + alias)).read(classes(application));        
+	}
+
+    @GET
+    @Produces({MediaType.APPLICATION_JSON, "application/yaml"})
+    @ApiOperation(value = "The swagger definition in either JSON or YAML", hidden = true)
+    @Path("/{alias}.{type:json|yaml}")
+    public Response getListing(@PathParam("alias") String alias, @PathParam("type") String type) {
+        if (type != null && type.trim().equalsIgnoreCase("yaml")) {
+            return getListingYaml(alias);
+        } else {
+            return getListingJson(alias);
+        }
+    }
+
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    @Path("/{alias}")
+    @ApiOperation(value = "The swagger definition in JSON", hidden = true)
+    public Response getListingJson(@PathParam("alias") String alias) {
+        return process(alias)
+        	.map(swagger -> Response.ok().entity(swagger))
+        	.orElseGet(() -> Response.status(Status.NOT_FOUND))
+        	.build();
+    }
+
+    @GET
+    @Produces("application/yaml")
+    @Path("/{alias}")
+    @ApiOperation(value = "The swagger definition in YAML", hidden = true)
+    public Response getListingYaml(@PathParam("alias") String alias) {
+    	return process(alias)
+            .map(this::asYamlString)
+            .map(s -> Response.ok().entity(s).type("application/yaml"))
+            .orElseGet(() -> Response.status(Status.NOT_FOUND))
+            .build();        
+    }
+    
+    private Set<Class<?>> classes(Application application) {
+    	return Stream.of(
+    			Stream.of(application.getClass()), 
+    			application.getClasses().stream(), 
+    			application.getSingletons().stream().map(Object::getClass))
+    		.flatMap(Function.identity())    		
+    		.collect(Collectors.toSet());
+    }
+
+    private String asYamlString(Swagger swagger)  {
+		try {
+			return Yaml.mapper().writeValueAsString(swagger);
+		} catch (JsonProcessingException e) {
+			throw new RuntimeException(e);
+		}
+    }
+}
---
+++ com.amplifino.nestor.swagger/src/com/amplifino/nestor/swagger/SwaggerApplication.java
@@ -0,0 +1,64 @@
+package com.amplifino.nestor.swagger;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.ws.rs.core.Application;
+
+import org.glassfish.hk2.utilities.binding.AbstractBinder;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.osgi.service.http.whiteboard.HttpWhiteboardConstants;
+
+import io.swagger.jaxrs.listing.SwaggerSerializers;
+
+@Component(service=Application.class, property={"alias=/swagger",
+		HttpWhiteboardConstants.HTTP_WHITEBOARD_RESOURCE_PATTERN + "=/apps/swagger/*",
+		HttpWhiteboardConstants.HTTP_WHITEBOARD_RESOURCE_PREFIX + "=/resources/web"})
+public class SwaggerApplication extends Application {
+	
+	private final Map<String, Application> applications = new ConcurrentHashMap<>();
+
+	@Reference(cardinality=ReferenceCardinality.AT_LEAST_ONE, policy=ReferencePolicy.DYNAMIC, target="(&(alias=/*)(!(alias=/swagger)))") 
+	public void addApplication(Application application, Map<String, Object> properties) {
+		Object alias = properties.get("alias");
+		if  (alias == null || ! (alias instanceof String)) {
+			return;
+		}
+		applications.put((String) alias, application);
+	}
+	
+	public void removeApplication(Application application, Map<String, Object> properties) {
+		applications.remove(properties.get("alias"), application);
+	}
+	
+	@Override
+	public Set<Class<?>> getClasses() {
+		SwaggerSerializers.setPrettyPrint(true);
+		Set<Class<?>> resources = new HashSet<>();
+		resources.add(ApiListing.class);
+		resources.add(SwaggerSerializers.class);
+		return resources;
+	}
+	
+	@Override
+	public Set<Object> getSingletons() {
+		return Collections.singleton(new AbstractBinder() {
+			@Override
+			protected void configure() {
+				bind(SwaggerApplication.this).to(SwaggerApplication.class);		
+			}
+		});
+	}
+
+	Optional<Application> getApplication(String alias) {
+		return Optional.ofNullable(applications.get("/" + alias));
+	}
+	
+}
